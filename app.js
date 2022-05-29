f89a988541ceb16aa12f1edacfac1dd0ebc40b4b
Anibal <anibal.gatica@mail.udp.cl>
2022-05-29 13:54:32 +0100

    test route

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
+});
+app.get("/login", function (req, res) {
+  res.sendFile(__dirname + "/data/users.json");
 });
 app.listen(port, function () {
   console.log(`Server started succefully in ${port}`);
