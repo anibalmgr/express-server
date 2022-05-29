68b73d20025cbad9a2eb05a30b662379c873378d
Anibal <anibal.gatica@mail.udp.cl>
2022-05-29 16:40:52 +0100

    set up cross

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 3001;
-app.get("/", function (req, res) {
+
+app.get("/", function (__, res) {
   res.sendFile(__dirname + "/index.html");
 });
-app.get("/login", function (req, res) {
+app.get("/user", function (__, res) {
+  res.set("Access-Control-Allow-Origin", "*");
   res.sendFile(__dirname + "/data/users.json");
 });
+
 app.listen(port, function () {
   console.log(`Server started succefully in ${port}`);
 });
