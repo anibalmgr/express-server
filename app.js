59d9827b4cb555025a98fe190da71093b462dee6
Anibal <anibal.gatica@mail.udp.cl>
2022-05-29 12:16:58 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 3000;
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+app.listen(port, function () {
+  console.log(`Server started succefully in ${port}`);
+});
