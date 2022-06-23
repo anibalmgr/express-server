f13266a99ee4df37cf16d98d6e785530bd5ff90f
anibal gatica <anibal.gatica@mail.udp.cl>
2022-06-23 20:24:12 +0100

    Sending cookie

--- app.js
+++ app.js
@@ -6,16 +6,18 @@ const User = require("./model/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const auth = require("./middleware/auth");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 app.use(express.json());
-
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cookieParser());
 
 app.post("/register", async (req, res) => {
   try {
     const { first_name, last_name, email, password } = req.body;
+    console.log(password);
 
     if (!(email && password && first_name && last_name)) {
       res.status(400).send("All inputs are required");
@@ -54,12 +56,15 @@ app.post("/register", async (req, res) => {
 });
 app.post("/login", async (req, res) => {
   try {
-    const { username, password } = req.body.username;
+    const { email, password } = req.body;
+    console.log(password);
 
     if (!(email && password)) {
       res.status(400).send("All inputs are required");
     }
-    const user = User.findOne({ username });
+    const user = await User.findOne({ email });
+    let compare = await bcrypt.compare(password, user.password);
+    console.log("compare", compare);
 
     if (user && (await bcrypt.compare(password, user.password))) {
       const token = jwt.sign(
@@ -71,10 +76,17 @@ app.post("/login", async (req, res) => {
       );
       user.token = token;
 
-      res.status(200).json(user);
+      res
+        .status(200)
+        .cookie("access_token", token, {
+          httpOnly: true,
+          // TODO: Check the right options
+          // secure: process.env.NODE_ENV === "production",
+        })
+        .json({ user: user.email });
+    } else {
+      res.status(400).send("Invalid Credentials");
     }
-
-    return res.status(400).send("Invalid Credentials");
   } catch (err) {
     console.log(err);
   }
@@ -93,14 +105,6 @@ app.get("/images/:image", (req, res) => {
 });
 app.get("/products/:id", (__, res) => {
   res.sendFile(__dirname + `/data/product.json`);
-});
-// Register
-app.post("/register", (req, res) => {
-  // our register logic goes here...
-});
-// Login
-app.post("/login", (req, res) => {
-  // our login logic goes here
 });
 
 module.exports = app;
